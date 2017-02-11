f1c03560a491d832dc5975918ce1d8dc970a84c5
Yuriy Kochetov <ribbedcrown@gmail.com>
2017-02-11 19:56:27 +0700

    Downgrade fs to be compatible with phoenix

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule StdJsonIo.Mixfile do
       {:porcelain, "~> 2.0"},
       {:poolboy, "~> 1.5.1"},
       {:poison, "~> 2.2.0"},
-      {:fs, "~> 2.12.0"},
+      {:fs, "~> 0.9.1"},
     ]
   end
 
